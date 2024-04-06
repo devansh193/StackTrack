214c37eba1523f31e761dfc1544c7e59725c6e6e
devansh <devanshverma193@gmail.com>
2024-04-06 14:58:01 +0530

    server action

--- src/app/(platform)/(dashboard)/_components/Sidebar.tsx
+++ src/app/(platform)/(dashboard)/_components/Sidebar.tsx
@@ -8,9 +8,6 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
-import { useState } from "react";
-import { string } from "zod";
-import { Pragati_Narrow } from "next/font/google";
 import { NavItem, Organization } from "./nav-item";
 
 
@@ -43,7 +40,15 @@ export const Sidebar = ({
     if(!isLoadedOrganization || !isLoadedOrgList || userMemberships.isLoading){
         return(
             <>
-            <Skeleton/>
+            <div className="flex items-center justify-between mb-2">
+            <Skeleton className="h-10 w-[50%]"/>
+            <Skeleton className="h-10 w-10"/>
+            </div>
+            <div className="space-y-2">
+                <NavItem.Skeleton/>
+                <NavItem.Skeleton/>
+                <NavItem.Skeleton/>
+            </div>
             </>
         )
     }
--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,33 @@
-const organizationIdPage = () =>{
+import { db } from "@/lib/db";
+
+import {create} from "@/actions/create-Board"
+import { Button } from "@/components/ui/button";
+
+const organizationIdPage = async () =>{
+    const boards = await db.board.findMany();
     return (
-        <div>
-            Organization page
+        <div className="flex flex-col space-y-4">
+            <form action={create}>
+                <input 
+                id="title"
+                name="title"
+                required
+                placeholder="Enter a board title"
+                className="border-black border p-2"
+                />
+                <Button type="submit">Submit</Button>
+                <form>
+                    <div className="space-y-2">
+                    {boards.map((board)=>(
+                        <div key={board.id}>
+                            board title: {board.title}
+                        </div>
+                    ))}
+                    </div>
+                </form>
+            </form>
         </div>
     );
-};
+    };
 
 export default organizationIdPage;
